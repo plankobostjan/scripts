97b104f824792b2ce287967bdc98e1dc3d73c782
Bostjan Planko <bostjan.planko@dhh.si>
2021-08-26 09:22:53 +0200

    bugfix
    Scritp was exiting after single event/commit. Now this is fixed. Script runs indefinitely.

--- inotify-committer/inotify-commit.sh
+++ inotify-committer/inotify-commit.sh
@@ -2,9 +2,10 @@
 
 DATA="$HOME/self $HOME/.ssh/known_hosts $HOME/.ssh/config $HOME/.functions $HOME/.aliases $HOME/.zshrc"
 
-while inotifywait -q -e close_write $DATA
+while true
 do
     cd $HOME
+    inotifywait -q -e close_write $DATA
     git add .
     git commit -m 'autocommit on change' -m 'Modified files: %w'
     git push origin master
