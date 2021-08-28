25323c740c7cc814753b3d4c203f77219a61416c
plankobostjan <bostjan@bostjanplanko.com>
2021-08-28 07:05:14 +0200

    big improvements
    Now it works mostly as it should. :)

--- inotify-committer/inotify-commit.sh
+++ inotify-committer/inotify-commit.sh
@@ -1,12 +1,38 @@
 #! /bin/bash
 
-DATA="$HOME/self $HOME/.ssh/known_hosts $HOME/.ssh/config $HOME/.functions $HOME/.aliases $HOME/.zshrc"
+WD="$(pwd)"
 
-while true
-do
-    MODIFIED_FILE=$(inotifywait -q -e close_write,modify,create,delete $DATA | awk '{ print $1; }')
-    cd $HOME
-    git add $MODIFIED_FILE
-    git commit -m "$(curl -s whatthecommit.com/index.txt)"
-    git push origin master
-done
+get_watchlist(){
+    WATCHLIST=""
+    if [ -e $WD/.gitignore ]; then
+        FILE="$WD/.gitignore"
+        while read -r LINE; do
+            #if [[ "$LINE" =~ ^!.*$  ]] && [[ -e $WD/${LINE:1} ]]; then
+            if [[ "$LINE" =~ ^!.*$  ]]; then
+                WATCHLIST="$WATCHLIST ${LINE:1}"
+            fi
+        done < "$FILE"
+    else
+        WATCHLIST="$WD"
+    fi
+    echo "$WATCHLIST"
+}
+
+main(){
+    while true
+    do
+        MODIFIED_FILE=$(inotifywait -q -e modify,create,delete --format='%f' $WD)
+        get_watchlist
+        echo "$MODIFIED_FILE"
+        if [[ "$WATCHLIST" =~ $MODIFIED_FILE ]]; then
+            echo "File $MODIFIED_FILE was modified."
+            cd $WD
+            git add $MODIFIED_FILE
+            git commit -m "$(curl -s whatthecommit.com/index.txt)"
+            git push origin main
+        fi
+    done
+}
+
+get_watchlist
+main
